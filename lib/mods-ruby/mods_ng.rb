c147c6fcaa01308284360672695fff9e9b9eddc5
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2010-02-04 18:15:21 -0600

    adding #role builder

--- lib/mods-ruby/mods_ng.rb
+++ lib/mods-ruby/mods_ng.rb
@@ -16,19 +16,33 @@ class ModsNG < Nokogiri::XML::Document
         
         # Adds role, accepting either :role=>"value" or :role=>{:value=>"value", :opt1=>"opt"}
         if opts[:role] 
-          xml.role {
-            if opts[:role].kind_of? Hash
-              role_opts = {:type=>"text"}.merge! opts[:role]
-              xml.roleTerm(role_opts.delete(:value), role_opts) 
-            else
-              xml.roleTerm(opts[:role], :type=>"text")
-            end
-          }
+          if opts[:role].kind_of? Hash
+            self.role(opts[:role].delete(:value), opts[:role], xml.parent)
+          else
+            self.role(opts[:role], {}, xml.parent)
+          end
         end
         
       }
     end
     return builder.doc
+  end
+  
+  def self.role text, opts={}, root_node=nil
+    opts = {:type=>"text"}.merge! opts
+    if root_node
+      builder = Nokogiri::XML::Builder.with(root_node) do |xml|
+        xml.role {
+          xml.roleTerm(text, opts) 
+        }
+      end
+    else
+      builder = Nokogiri::XML::Builder.new do |xml|
+        xml.role {
+          xml.roleTerm(text, opts) 
+        }
+      end
+    end
   end
   
 end
--- spec/unit/mods_ng_spec.rb
+++ spec/unit/mods_ng_spec.rb
@@ -12,17 +12,40 @@ describe "ModsNG" do
   end
   
   describe "#name" do
-    n1 = ModsNG.name("Beethoven, Ludwig van", :date=>"1770-1827")
-    n1.to_xml.should == Nokogiri::XML.parse('<name type="personal"><namePart>Beethoven, Ludwig van</namePart><namePart type="date">1770-1827</namePart></name>').to_xml
+    it "should construct name nodes" do
+      n1 = ModsNG.name("Beethoven, Ludwig van", :date=>"1770-1827")
+      n1.to_xml.should == Nokogiri::XML.parse('<name type="personal"><namePart>Beethoven, Ludwig van</namePart><namePart type="date">1770-1827</namePart></name>').to_xml
     
-    n2 = ModsNG.name("Naxos Digital Services", :type=>"corporate")
-    n2.to_xml.should == Nokogiri::XML.parse('<name type="corporate"><namePart>Naxos Digital Services</namePart></name>').to_xml
+      n2 = ModsNG.name("Naxos Digital Services", :type=>"corporate")
+      n2.to_xml.should == Nokogiri::XML.parse('<name type="corporate"><namePart>Naxos Digital Services</namePart></name>').to_xml
     
-    n3 = ModsNG.name("Alterman, Eric", :role=>"creator")
-    n3.to_xml.should == Nokogiri::XML.parse('<name type="personal"><namePart>Alterman, Eric</namePart><role><roleTerm type="text">creator</roleTerm></role></name>').to_xml
+      n3 = ModsNG.name("Alterman, Eric", :role=>"creator")
+      n3.to_xml.should == Nokogiri::XML.parse('<name type="personal"><namePart>Alterman, Eric</namePart><role><roleTerm type="text">creator</roleTerm></role></name>').to_xml
     
-    n4 = ModsNG.name("Tuell, Hiram.", :role=>{:value=>"creator", :authority=>"marcrelator"})
-    n4.to_xml.should == Nokogiri::XML.parse('<name type="personal"><namePart>Tuell, Hiram.</namePart><role><roleTerm type="text" authority="marcrelator">creator</roleTerm></role></name>').to_xml
+      n4 = ModsNG.name("Tuell, Hiram.", :role=>{:value=>"creator", :authority=>"marcrelator"})
+      n4.to_xml.should == Nokogiri::XML.parse('<name type="personal"><namePart>Tuell, Hiram.</namePart><role><roleTerm type="text" authority="marcrelator">creator</roleTerm></role></name>').to_xml
+    end
+  end
+  
+  describe '#role' do
+    it "should construct role nodes" do
+      r1 = ModsNG.role("creator")
+      r1.to_xml.should == Nokogiri::XML.parse('<role><roleTerm type="text">creator</roleTerm></role>').to_xml
+    
+      r2 = ModsNG.role("creator", :authority=>"marcrelator")
+      r2.to_xml.should == Nokogiri::XML.parse('<role><roleTerm type="text" authority="marcrelator">creator</roleTerm></role>').to_xml
+    end
+    it "should accept a root node to insert its results into" do
+      builder = Nokogiri::XML::Builder.new do |xml|
+        xml.foo {
+          xml.bar {
+            ModsNG.role("creator", {:authority=>"marcrelator"}, xml.parent)
+          }
+        }
+      end
+      builder.to_xml.should == Nokogiri::XML.parse('<foo><bar><role><roleTerm type="text" authority="marcrelator">creator</roleTerm></role></bar></foo>').to_xml
+      
+    end
   end
   
   describe "names" do
